44a8a97c5dd1953f7c76e6bf376b0c25e121b0d7
Babs <68171102+Ohubabs@users.noreply.github.com>
2023-12-08 16:44:52 -0800

    Added acm arn for testing

--- ingress.tf
+++ ingress.tf
@@ -2,12 +2,12 @@ resource "kubernetes_ingress_v1" "arsenal" {
   wait_for_load_balancer = true
   metadata {
     name = "arsenal"
-    namespace = "default"
+    namespace = "arsenal"
     annotations = {
         "alb.ingress.kubernetes.io/load-balancer-name" = "arsenal"
         "alb.ingress.kubernetes.io/scheme"             = "internet-facing"
         "alb.ingress.kubernetes.io/target-type"        = "ip" 
-        "alb.ingress.kubernetes.io/certificate-arn" = module.acm.acm_certificate_arn
+        "alb.ingress.kubernetes.io/certificate-arn" = "arn:aws:acm:us-west-2:083772204804:certificate/e773e406-a77d-4557-aeb2-f126b91cf6b6"
   }
   }
   spec {
