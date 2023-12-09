0d0c04b4a374e78b82ac41196ffd410b53ac1935
Babs <68171102+Ohubabs@users.noreply.github.com>
2023-12-08 18:24:42 -0800

    Update arsenal-svc.tf

--- arsenal-svc.tf
+++ arsenal-svc.tf
@@ -2,10 +2,10 @@ resource "kubernetes_service_v1" "arsenal-svc" {
   metadata {
     name = "arsenal-svc"
     #annotations = [
-                    service.beta.kubernetes.io/aws-load-balancer-type: "external" #To use a AWS NLB instead of CLB
-                    service.beta.kubernetes.io/aws-load-balancer-nlb-target-type: "ip"
-                    service.beta.kubernetes.io/aws-load-balancer-scheme:  internet-facing
-                    service.beta.kubernetes.io/aws-load-balancer-name: arsenal-nlb
+                    "service.beta.kubernetes.io/aws-load-balancer-type" = "external" #To use a AWS NLB instead of CLB
+                    "service.beta.kubernetes.io/aws-load-balancer-nlb-target-type" = "ip"
+                    "service.beta.kubernetes.io/aws-load-balancer-scheme" =  internet-facing
+                    "service.beta.kubernetes.io/aws-load-balancer-name" = arsenal-nlb
                   ]
   }
   spec {
