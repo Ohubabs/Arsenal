c8611a2f71166f368c9eca6bd8d84819b4a74f05
Babs <68171102+Ohubabs@users.noreply.github.com>
2023-12-08 17:47:05 -0800

    Update ingress.tf

--- ingress.tf
+++ ingress.tf
@@ -7,7 +7,7 @@ resource "kubernetes_ingress_v1" "arsenal" {
         "alb.ingress.kubernetes.io/load-balancer-name" = "arsenal"
         "alb.ingress.kubernetes.io/scheme"             = "internet-facing"
         "alb.ingress.kubernetes.io/target-type"        = "ip" 
-        "alb.ingress.kubernetes.io/certificate-arn" = "arn:aws:acm:us-west-2:083772204804:certificate/e773e406-a77d-4557-aeb2-f126b91cf6b6"
+        "alb.ingress.kubernetes.io/certificate-arn" =  "arn:aws:acm:us-west-2:083772204804:certificate/2f13a37f-77ef-4971-a3ad-70d6d09b9779"
   }
   }
   spec {
