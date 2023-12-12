dafb1da36560198ecf9a883319a5fb13d358f1d9
Babs <68171102+Ohubabs@users.noreply.github.com>
2023-12-12 11:20:13 -0800

    Update secret.tf and namespace.tf in one file

--- secret.tf
+++ secret.tf
@@ -1,13 +1,19 @@
-resource "kubernetes_secret_v1" "aws-auth" {
-  metadata {
-    name = "konoha"
-    namespace = "cert-manager"
-  }
+#resource "kubernetes_secret_v1" "aws-auth" {
+#  metadata {
+#    name = "konoha"
+#    namespace = "cert-manager"
+#  }
+#
+#  data = {
+#    access-key-id = "xxx"
+#    secret-access-key = "xxx"
+#  }
+#
+#  type = "Opaque"
+#}
 
-  data = {
-    access-key-id = "xxx"
-    secret-access-key = "xxx"
-  }
-
-  type = "Opaque"
-}
+#resource "kubernetes_namespace_v1" "certmanager-namespace" {
+#  metadata {
+#    name = "arsenal"
+#  }
+#}
