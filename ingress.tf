43007f09fe15eb8ee6b2bd499de00468afc89be2
Babs <68171102+Ohubabs@users.noreply.github.com>
2023-12-12 11:12:16 -0800

    Updated ALB ingress and Ngiinx ingress

--- ingress.tf
+++ ingress.tf
@@ -1,13 +1,51 @@
-resource "kubernetes_ingress_v1" "arsenal" {
+##NGINX
+
+#resource "kubernetes_ingress_v1" "arsenal-ingress" {
+#  wait_for_load_balancer = true
+#  metadata {
+#    name = "arsenal"
+#    namespace = "default"
+#    annotations = {
+#        "cert-manager.io/cluster-issuer" = "arsenal-issuer1"
+#  }
+#  }
+#  spec {
+#    ingress_class_name = "nginx"
+#    tls {
+#      secret_name = "arsenal-secret"
+#      hosts = ["arsenal.devopsnetwork.net"] 
+#    }
+#    rule {
+#      host = "arsenal.devopsnetwork.net"  
+#      http {
+#        path {
+#          path = "/"
+#          backend {
+#            service {
+#              name = "arsenal-svc"
+#              port {
+#                number = 80
+#              }
+#            }
+#          }
+#        }
+#      }
+#    }
+#  }
+#}
+
+##ALB
+
+resource "kubernetes_ingress_v1" "jenkins1" {
   wait_for_load_balancer = true
   metadata {
     name = "arsenal"
     namespace = "default"
     annotations = {
-        "alb.ingress.kubernetes.io/load-balancer-name": "arsenal"
-        "alb.ingress.kubernetes.io/scheme"            : "internet-facing"
-        "alb.ingress.kubernetes.io/target-type"       : "ip" 
-        "alb.ingress.kubernetes.io/certificate-arn"   :  "arn:aws:acm:us-west-2:083772204804:certificate/2f13a37f-77ef-4971-a3ad-70d6d09b9779"
+        "alb.ingress.kubernetes.io/load-balancer-name" = "arsenal"
+        "alb.ingress.kubernetes.io/scheme"             = "internet-facing"
+        "alb.ingress.kubernetes.io/target-type"        = "ip"                                       
+        "alb.ingress.kubernetes.io/certificate-arn" = "arn:aws:acm:us-west-2:083772204804:certificate/f8707613-028b-470b-bd2a-703f5ac1e73d"
   }
   }
   spec {
@@ -21,11 +59,11 @@ resource "kubernetes_ingress_v1" "arsenal" {
       }
     }
     rule {
-      host = "arsenal.skyways-hotel.com"  
+      host = "arsenal.devopsnetwork.net"  
       http {
         path {
           path = "/"
-          path_type = "Prefix"
+          path_type = "Exact"
           backend {
             service {
               name = "arsenal-svc"
